7429a04ac9aceb83f9589ef98b959cf33fce4131
Federico Luzzi <fedeluzzi00@gmail.com>
2021-05-27 14:04:58 +0200

    chore: fix build

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
@@ -13,7 +13,7 @@ module.exports = (config, env) => {
   config.output.filename = `static/js/[name].${shortCommitHash}.js`
   config.output.chunkFilename = `static/js/[name].${shortCommitHash}.chunk.js`
   config.plugins = config.plugins.filter(
-    plugin =>
+    (plugin) =>
       !(
         plugin instanceof WorkboxWebpackPlugin.GenerateSW ||
         plugin instanceof ManifestPlugin ||
@@ -23,12 +23,12 @@ module.exports = (config, env) => {
   config.plugins.push(
     new MiniCssExtractPlugin({
       filename: `static/css/[name].${shortCommitHash}.css`,
-      chunkFilename: 'static/css/[name].chunk.css'
+      chunkFilename: 'static/css/[name].chunk.css',
     })
   )
-  config.module.rules[2].oneOf.find(rule => rule.loader === require.resolve('file-loader')).options.name =
+  config.module.rules[1].oneOf.find((rule) => rule.loader === require.resolve('file-loader')).options.name =
     'static/media/[name].[ext]'
-  config.module.rules[2].oneOf.find(rule => rule.loader === require.resolve('url-loader')).options.name =
+  config.module.rules[1].oneOf.find((rule) => rule.loader === require.resolve('url-loader')).options.name =
     'static/media/[name].[ext]'
   config.optimization.moduleIds = 'hashed'
   return config
