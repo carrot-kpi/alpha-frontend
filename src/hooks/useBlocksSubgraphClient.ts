646c454db0d1c6a902f9b7d634df0926ab72df28
Federico Luzzi <fedeluzzi00@gmail.com>
2021-10-08 15:34:02 +0200

    fix: remove chain id override from use blocks subgraph client

--- src/hooks/useBlocksSubgraphClient.ts
+++ src/hooks/useBlocksSubgraphClient.ts
@@ -3,8 +3,8 @@ import { ChainId } from '@carrot-kpi/sdk'
 import { BLOCK_SUBGRAPH_CLIENTS } from '../constants/apollo'
 import { useActiveWeb3React } from './useActiveWeb3React'
 
-export function useBlocksSubgraphClient(chainIdOverride?: ChainId): ApolloClient<NormalizedCacheObject> {
+export function useBlocksSubgraphClient(): ApolloClient<NormalizedCacheObject> {
   const { chainId } = useActiveWeb3React()
   // FIXME: use mainnet as the default indexing key
-  return BLOCK_SUBGRAPH_CLIENTS[chainIdOverride || (chainId as ChainId) || ChainId.XDAI]
+  return BLOCK_SUBGRAPH_CLIENTS[(chainId as ChainId) || ChainId.XDAI]
 }
