f971a36048e30b3a1a2987b8af1acb9e7130ba6d
My Violet <milanrasovic.f@gmail.com>
2021-11-05 19:19:15 +0100

    feature: Style and various fixes

--- src/components/oracle/index.tsx
+++ src/components/oracle/index.tsx
@@ -1,104 +1,186 @@
 import { ReactElement, useEffect, useState } from 'react'
 import { Card } from '../card'
 import { Title } from '../title'
-// import { ExternalLink } from '../undecorated-link'
-// import { Text } from 'rebass'
 import { Button, Flex } from 'rebass'
 import { KpiToken } from '@carrot-kpi/sdk'
 
-import {BigNumberInput} from 'big-number-input'
+import { BigNumberInput } from 'big-number-input'
 import { isScalarQuestion, numberToByte32 } from '../../utils'
 import { INVALID_ANSWER_ID } from '../../constants'
 import { useRealityQuestion } from '../../hooks/useRealityQuestion'
 import Skeleton from 'react-loading-skeleton'
-import { formatEther } from '@ethersproject/units'
+import { formatEther, formatUnits } from '@ethersproject/units'
+import { Zero } from '@ethersproject/constants'
+import styled from 'styled-components'
+import { useNativeCurrency } from '../../hooks/useNativeCurrency'
 
-enum RealityBinary{
+enum RealityBinary {
   YES,
   NO,
+  INVALID,
 }
+const ExtraData = styled.div`
+  position: relative;
+  left: 28px;
+  font-weight: 500;
+  color: #979797;
+  font-size: 9px;
+  margin-bottom: 9px;
+`
+const StyledLabel = styled.label`
+  margin-left: 7px;
+`
+const StyledSubmitButton = styled(Button)`
+  border-radius: 6px !important;
+  font-weight: 700;
+  font-size: 14px;
+`
+const StyledInvalidButton = styled(StyledSubmitButton)`
+  border: 1px solid #ff782d !important;
+`
+const AdditionalScalarData = styled(ExtraData)`
+  margin-top: 5px;
+`
+export const Oracle = ({ kpi }: { kpi: KpiToken | undefined }): ReactElement => {
+  const [isScalar, setIsScalar] = useState(true)
+  const [loader, setLoader] = useState(true)
+  const { submitAnswer, transactionLoader, currentAnswer } = useRealityQuestion(kpi?.kpiId)
 
+  const [bigNumberValue, setBigNumberValue] = useState('')
+  const [radioValue, setRadioValue] = useState<RealityBinary>(RealityBinary.YES)
+  const { decimals, symbol } = useNativeCurrency()
 
-export const Oracle = ({ kpi }: { kpi: KpiToken | undefined}): ReactElement => {
-      const [isScalar,setIsScalar]=useState(true)
-  const [loader,setLoader]=useState(true)
-  const {submitAnswer, transactionLoader,currentBond}=useRealityQuestion(kpi?.kpiId)
+  useEffect(() => {
+    if (!kpi) setLoader(true)
+    else setIsScalar(isScalarQuestion(kpi.lowerBound, kpi.higherBound)), setLoader(false)
+  }, [kpi])
 
-  const [bigNumberValue,setBigNumberValue]=useState('')
-  const [radioValue,setRadioValue]=useState<RealityBinary>(RealityBinary.YES)
-
-  useEffect(()=>{
-    if(!kpi) setLoader(true)
-    else setIsScalar(isScalarQuestion(kpi.lowerBound,kpi.higherBound)),setLoader(false)
-
-  },[kpi])
-
-
-const handelRadioChange=(value:any)=>{
-        const target=value.target.value
-        setRadioValue(parseInt(target))
-}
-const intiaiteSubmit=async (isInvalid:boolean)=>{
-  const answer =
-     isInvalid
-      ? INVALID_ANSWER_ID
-      : numberToByte32(isScalar ?bigNumberValue : radioValue)
-  const tx=await submitAnswer(answer)
-  console.log(tx)
-
-
-
-}
+  const handelRadioChange = (value: any) => {
+    const target = value.target.value
+    setRadioValue(parseInt(target))
+  }
+  const intiaiteSubmit = async (isInvalid = false) => {
+    const answer =
+      isInvalid || radioValue === RealityBinary.INVALID
+        ? INVALID_ANSWER_ID
+        : numberToByte32(isScalar ? bigNumberValue : radioValue)
+    await submitAnswer(answer)
+  }
   return (
-      <Card flexDirection="column" m="8px">
+    <Card flexDirection="column" m="8px">
+      <Title mb="8px">Oracle</Title>
 
-        <Title mb="8px">Oracle</Title>
-
-        { isScalar?
-    <>
-      {loader?<Skeleton width={'80px'}/>:<BigNumberInput decimals={18} value={bigNumberValue} onChange={setBigNumberValue}/>}
-
+      {isScalar ? (
+        <>
+          {loader ? (
+            <Skeleton width={'80px'} />
+          ) : (
+            <>
+              <BigNumberInput decimals={18} value={bigNumberValue} onChange={setBigNumberValue} />
+              {!currentAnswer.bond.eq(Zero) && (
+                <AdditionalScalarData>
+                  {formatUnits(currentAnswer.bond, decimals)} {symbol} bonded on{' '}
+                  {currentAnswer.answer === INVALID_ANSWER_ID ? 'Invalid' : formatEther(currentAnswer.answer)}
+                </AdditionalScalarData>
+              )}
             </>
+          )}
+        </>
+      ) : (
+        <form>
+          {loader ? (
+            <Flex flexDirection={'column'}>
+              <Skeleton width={'80px'} />
+              <Skeleton width={'80px'} />
+            </Flex>
+          ) : (
+            <>
+              <div>
+                <input
+                  type="radio"
+                  name="react-tips"
+                  value={RealityBinary.YES}
+                  checked={radioValue === RealityBinary.YES}
+                  onChange={handelRadioChange}
+                />
+                <StyledLabel>Reached</StyledLabel>
 
-        : <form>
-            {loader?<Flex flexDirection={'column'}>
-              <Skeleton width={'80px'}/>
-              <Skeleton width={'80px'}/>
-            </Flex>: <> <div className="form-check">
-              <label>
-              <input
-              type="radio"
-              name="react-tips"
-              value={RealityBinary.YES}
-              checked={radioValue===RealityBinary.YES}
-              className="form-check-input"
-              onChange={handelRadioChange}
-              />
-              Reached {formatEther(currentBond)}
-              </label>
+                <ExtraData>
+                  {!currentAnswer.bond.eq(Zero) &&
+                    currentAnswer.answer === '0x0000000000000000000000000000000000000000000000000000000000000000' &&
+                    `${formatEther(currentAnswer.bond)} ${symbol} Bonded`}
+                </ExtraData>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="react-tips"
+                  value={RealityBinary.NO}
+                  checked={radioValue === RealityBinary.NO}
+                  onChange={handelRadioChange}
+                />
+                <StyledLabel>Not Reached</StyledLabel>
+
+                <ExtraData>
+                  {!currentAnswer.bond.eq(Zero) &&
+                    currentAnswer.answer === '0x0000000000000000000000000000000000000000000000000000000000000001' &&
+                    `${formatEther(currentAnswer.bond)} ${symbol} Bonded`}
+                </ExtraData>
               </div>
               <div className="form-check">
-              <label>
-              <input
-              type="radio"
-              name="react-tips"
-              value={RealityBinary.NO}
-              className="form-check-input"
-              checked={radioValue===RealityBinary.NO}
-              onChange={handelRadioChange}
-              />
-              Not Reached
-              </label>
-              </div></>}
-          </form>}
-        {transactionLoader?<Skeleton width={'100%'}/>:
-          <>
-            <Button backgroundColor='#d66766' onClick={()=>{intiaiteSubmit(false)}}>Submit Answer</Button>
-            <Button backgroundColor='#d66700' onClick={()=>{intiaiteSubmit(true)}}>Set Invalid</Button>
-          </>}
+                <input
+                  type="radio"
+                  name="react-tips"
+                  value={RealityBinary.INVALID}
+                  checked={radioValue === RealityBinary.INVALID}
+                  onChange={handelRadioChange}
+                />
 
+                <StyledLabel>Invalid</StyledLabel>
 
-      </Card>
-
+                <ExtraData>
+                  {!currentAnswer.bond.eq(Zero) &&
+                    currentAnswer.answer === INVALID_ANSWER_ID &&
+                    `${formatEther(currentAnswer.bond)} ${symbol} Bonded`}
+                </ExtraData>
+              </div>
+            </>
+          )}
+        </form>
+      )}
+      {transactionLoader ? (
+        <Skeleton width={'100%'} />
+      ) : (
+        <>
+          <StyledSubmitButton
+            backgroundColor="#FF782D"
+            marginTop={'9px'}
+            onClick={() => {
+              intiaiteSubmit()
+            }}
+          >
+            Bond{' '}
+            {currentAnswer.bond.gt(Zero)
+              ? `${formatUnits(currentAnswer.bond.mul(2), decimals)} ${symbol}`
+              : `0.01 ${symbol}`}
+          </StyledSubmitButton>
+          {isScalar && (
+            <StyledInvalidButton
+              color="#FF782D"
+              marginTop={'9px'}
+              backgroundColor="white"
+              onClick={() => {
+                intiaiteSubmit(true)
+              }}
+            >
+              Invalid{' '}
+              {currentAnswer.bond.gt(Zero)
+                ? `${formatUnits(currentAnswer.bond.mul(2), decimals)} ${symbol}`
+                : `0.01 ${symbol}`}
+            </StyledInvalidButton>
+          )}
+        </>
+      )}
+    </Card>
   )
 }
--- src/hooks/useRealityQuestion.ts
+++ src/hooks/useRealityQuestion.ts
@@ -2,51 +2,55 @@ import { useEffect, useMemo, useState } from 'react'
 import { useSingleCallResult } from '../state/multicall/hooks'
 import { useRealityContract } from './useContract'
 import { useActiveWeb3React } from './useActiveWeb3React'
-import {  parseUnits } from '@ethersproject/units'
+import { parseUnits } from '@ethersproject/units'
 import { Zero } from '@ethersproject/constants'
 import { NETWORK_DETAIL } from '../constants'
 import { BigNumber } from '@ethersproject/bignumber'
 
-
-export function useRealityQuestion(kpiId:string | undefined): { transactionLoader: boolean; submitAnswer: (answer: string) => Promise<void>;currentBond:BigNumber } {
+export function useRealityQuestion(kpiId: string | undefined): {
+  transactionLoader: boolean
+  submitAnswer: (answer: string) => Promise<void>
+  currentAnswer: { answer: string; bond: BigNumber }
+} {
   const realityContract = useRealityContract(true)
   const callParams = useMemo(() => [kpiId], [kpiId])
-  const bondAmount = useSingleCallResult(realityContract, 'getBond', callParams)
-  const { library:provider,chainId } = useActiveWeb3React()
-  const [currentBond,setCurrentBond]=useState<BigNumber>(Zero)
-
-  console.log(realityContract)
-  useEffect(()=>{
-    if(!kpiId) return
-    console.log('question id ',kpiId)
-    setCurrentBond(bondAmount && bondAmount.result ? bondAmount.result[0]:Zero)
-
-  },[kpiId])
-
+  const questionData = useSingleCallResult(realityContract, 'questions', callParams)
+  const { library: provider, chainId } = useActiveWeb3React()
   const [transactionLoader, setTransactionLoader] = useState(false)
+  const [currentAnswer, setCurrentAnswer] = useState<{ answer: string; bond: BigNumber }>({ answer: '', bond: Zero })
 
-  const submitAnswer=async (answer:string)=>{
-    try{
+  useEffect(() => {
+    if (!kpiId) return
+    console.log('data I need', questionData)
+    setCurrentAnswer(
+      questionData && questionData.result
+        ? { answer: questionData.result['best_answer'], bond: questionData.result.bond }
+        : { answer: '', bond: Zero }
+    )
+  }, [kpiId, questionData])
+
+  const submitAnswer = async (answer: string) => {
+    try {
       setTransactionLoader(true)
-        if(realityContract && bondAmount  && provider && !bondAmount.error && chainId){
-          const currentNetwork=NETWORK_DETAIL[chainId]
-          const initialBondAmount =
-            currentNetwork.chainName==='xDai'  ? parseUnits('10', currentNetwork.nativeCurrency.decimals) : parseUnits('0.01', currentNetwork.nativeCurrency.decimals)
-          const bond=bondAmount.result && !bondAmount.result[0].eq(Zero)?bondAmount.result[0].mul(2):initialBondAmount
-          const signer=provider.getSigner()
-          const contract=realityContract.connect(signer)
-          const txRecepir=await contract.submitAnswer(kpiId,answer,0,{value:bond})
-
-          console.log(txRecepir)
-        }
-
-
-    }catch (e) {
+      if (realityContract && questionData && provider && !questionData.error && chainId) {
+        const currentNetwork = NETWORK_DETAIL[chainId]
+        const initialquestionData =
+          currentNetwork.chainName === 'xDai'
+            ? parseUnits('10', currentNetwork.nativeCurrency.decimals)
+            : parseUnits('0.01', currentNetwork.nativeCurrency.decimals)
+        const bond =
+          questionData.result && !questionData.result[6].eq(Zero) ? questionData.result[6].mul(2) : initialquestionData
+        const txRecepir = await realityContract.submitAnswer(kpiId, answer, 0, { value: bond })
+        console.log(txRecepir)
+        await provider.waitForTransaction(txRecepir.hash)
+        console.log(txRecepir)
+        setTransactionLoader(false)
+      }
+    } catch (e) {
       console.log(e)
       setTransactionLoader(false)
     }
   }
 
-
-  return {  transactionLoader, submitAnswer,currentBond }
+  return { transactionLoader, submitAnswer, currentAnswer }
 }
