67c26f0dd6ecd194182a3b22fd368a4690c4f870
Federico Luzzi <fedeluzzi00@gmail.com>
2021-11-26 17:20:41 +0100

    chore: remove link property from creator interface

--- src/constants/creators/index.ts
+++ src/constants/creators/index.ts
@@ -3,11 +3,9 @@ import dxdaoLogo from '../../assets/svgs/dxdao-logo.svg'
 export interface Creator {
   name: string
   logo: string
-  link: string
 }
 
 export const DXDAO: Creator = {
   name: 'DXdao',
   logo: dxdaoLogo,
-  link: 'https://dxdao.eth.link',
 }
