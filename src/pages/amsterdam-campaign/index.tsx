0b3c33fef6567da30fbd10263b69a32472278481
rossneilson <rossneilson99@gmail.com>
2022-04-18 22:37:38 +0100

    feat: Adding ipfs url and removing hash router

--- src/pages/amsterdam-campaign/index.tsx
+++ src/pages/amsterdam-campaign/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useEffect } from 'react'
 
 export function AmsterdamCampaign(): ReactElement {
   useEffect(() => {
-    window.location.href = 'https://amsterdam.carrot.eth.limo'; 
+    window.location.href = 'https://bafybeidiuwfehkbmvopteccr63qd2sidjtgchc4m3enj2o4a3nyluepzvm.ipfs.dweb.link/'; 
   }, []);
 
   return <></>
